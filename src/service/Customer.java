fdf40d83c2422421ad75a11e237feb45eb5c132e
badre sodki <bsodki@sqli.com>
2018-03-28 16:22:52 +0100

    1 commit

---
+++ src/service/Customer.java
@@ -0,0 +1,7 @@
+package service;
+
+import java.util.Collection;
+
+public class Customer extends Person {
+
+}
---
+++ src/service/Product.java
@@ -0,0 +1,31 @@
+package service;
+
+public class Product {
+
+    private String name;
+    private String id;
+
+    public Product() {
+    }
+
+    public Product(String name, String id) {
+        this.name = name;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
